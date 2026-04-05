c02f3748b62aca95b1cd74a2c21cd153c09a6595
leenshhadeh <110395364+leenshhadeh@users.noreply.github.com>
2026-04-05 15:19:29 +0300

    Feature/add tags select (#17)
    * add tag select
    
    * add users select

--- src/features/module-assessment-data/constants/CurrentApplication.ts
+++ src/features/module-assessment-data/constants/CurrentApplication.ts
@@ -5,6 +5,24 @@ export const ASSESSMENT_APPLICATIONS = [
 {id: 'app4', name: 'Application 4'},
 {id: 'app5', name: 'Application 5'},
 {id: 'app6', name: 'Application 6'},
+{id: 'app7', name: 'Application 7'},
+{id: 'app8', name: 'Application 8'},
+{id: 'app9', name: 'Application 9'},
+{id: 'app10', name: 'Application 10'},
+{id: 'app11', name: 'Application 11'},
+{id: 'app12', name: 'Application 12'},
+{id: 'app13', name: 'Application 13'},
+{id: 'app14', name: 'Application 14'},
+{id: 'app15', name: 'Application 15'},
 
+
+  ]
+
+  export const DIGITAL_FP_USERS= [
+    {id: 'user1', name: 'John Doe', img: 'https://t4.ftcdn.net/jpg/06/45/77/79/360_F_645777959_fNnaNoeVO4qxCNPW9MWr3gQlPFSGA9yL.jpg'},
+    {id: 'user2', name: 'Jane Smith' , img: 'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png'},
+    {id: 'user3', name: 'Alice Johnson' , img: 'https://t4.ftcdn.net/jpg/06/45/77/79/360_F_645777959_fNnaNoeVO4qxCNPW9MWr3gQlPFSGA9yL.jpg'},
+    {id: 'user4', name: 'Bob Brown' , img: 'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png'},
+    {id: 'user5', name: 'Charlie Davis' , img: ''},
   ]
   
