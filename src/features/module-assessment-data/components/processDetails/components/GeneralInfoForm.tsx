54d5f90fc121834248122be72b3a247ba1dc6746
leenshhadeh <leen.shehadeh@pwc.com>
2026-04-20 12:50:53 +0300

    handel comments hover state

--- src/features/module-assessment-data/components/processDetails/components/GeneralInfoForm.tsx
+++ src/features/module-assessment-data/components/processDetails/components/GeneralInfoForm.tsx
@@ -6,7 +6,7 @@ import { cn } from '@/shared/lib/utils'
 import commentIcon from '@/assets/icons/Comment-circle.svg'
 
 const GeneralInfoForm = (props: any) => {
-  const { onFormSubmit, onFormChanged, initialData, isEditable, isUserAuthToComment ,showComments} = props
+  const { onFormSubmit, onFormChanged, initialData, isEditable, canComment, showComments } = props
 
   const [formData, setFormData] = useState({
     customName: initialData.customName || '',
@@ -29,10 +29,14 @@ const GeneralInfoForm = (props: any) => {
         className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3"
         onSubmit={onFormSubmit}
       >
-        <div className="flex w-full flex-col">
+        <div className="group flex w-full flex-col">
           <div className="flex items-center justify-between">
             <label className="text-muted-foreground text-sm">Custom Name</label>
-            {isUserAuthToComment && <img src={commentIcon} width={'23px'} className="mb-1" onClick={showComments}/>}
+            {canComment && (
+              <div className="opacity-0 transition-opacity group-hover:opacity-100">
+                <img src={commentIcon} width={'23px'} className="mb-1" onClick={()=>showComments('customName')} />
+              </div>
+            )}
           </div>
           <Input
             name="customName"
@@ -43,8 +47,16 @@ const GeneralInfoForm = (props: any) => {
           />
         </div>
 
-        <div className="flex w-full flex-col">
+        <div className="group flex w-full flex-col">
+          
+          <div className="flex items-center justify-between">
           <label className="text-muted-foreground text-sm">Custom Description</label>
+            {canComment && (
+              <div className="opacity-0 transition-opacity group-hover:opacity-100">
+                <img src={commentIcon} width={'23px'} className="mb-1" onClick={()=>showComments('customDescription')} />
+              </div>
+            )}
+          </div>
           <textarea
             name="customDescription"
             value={formData.customDescription}
@@ -54,8 +66,16 @@ const GeneralInfoForm = (props: any) => {
           />
         </div>
 
-        <div className="flex w-full flex-col">
+        <div className="flex w-full flex-col group">
+          
+          <div className="flex items-center justify-between">
           <label className="text-muted-foreground text-sm">Process Description</label>
+            {canComment && (
+              <div className="opacity-0 transition-opacity group-hover:opacity-100">
+                <img src={commentIcon} width={'23px'} className="mb-1" onClick={()=>showComments('processDescription')} />
+              </div>
+            )}
+          </div>
           <textarea
             name="processDescription"
             value={formData.processDescription}
@@ -65,8 +85,15 @@ const GeneralInfoForm = (props: any) => {
           />
         </div>
 
-        <div className="flex w-full flex-col">
+        <div className="flex w-full flex-col group">
+          <div className="flex items-center justify-between">
           <label className="text-muted-foreground text-sm">Responsible Business Focal Point</label>
+            {canComment && (
+              <div className="opacity-0 transition-opacity group-hover:opacity-100">
+                <img src={commentIcon} width={'23px'} className="mb-1" onClick={()=>showComments('responsibleBusinessFocalPoint')} />
+              </div>
+            )}
+          </div>
           <div
             className={cn(
               'border-border bg-background text-foreground placeholder:text-muted-foreground focus-visible:ring-ring/40 flex h-auto w-full min-w-0 rounded-md border p-2 text-sm shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-3',
@@ -87,8 +114,15 @@ const GeneralInfoForm = (props: any) => {
           </div>
         </div>
 
-        <div className="flex w-full flex-col">
+        <div className="flex w-full flex-col group">
+          <div className="flex items-center justify-between">
           <label className="text-muted-foreground text-sm">Responsible Digital Focal Point</label>
+            {canComment && (
+              <div className="opacity-0 transition-opacity group-hover:opacity-100">
+                <img src={commentIcon} width={'23px'} className="mb-1" onClick={()=>showComments('responsibleDigitalFocalPoint')} />
+              </div>
+            )}
+          </div>
           <div
             className={cn(
               'border-border bg-background text-foreground placeholder:text-muted-foreground focus-visible:ring-ring/40 flex h-auto w-full min-w-0 rounded-md border p-2 text-sm shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-3',
